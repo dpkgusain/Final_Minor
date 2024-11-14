6d9060b1eb7293ccda0ddd97156a9b0164b80809
Deepak <dpkgusain45@gmail.com>
2024-11-14 21:24:55 +0530

    14 Nov night

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import {
-    FaFacebookF,
-    FaInstagram,
-    FaTwitter,
-    FaLinkedin,
-    FaGithub,
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+  FaGithub,
 } from "react-icons/fa";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
@@ -12,41 +12,50 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import "./style.scss";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <ContentWrapper>
-                <ul className="menuItems">
-                    <li className="menuItem">Terms Of Use</li>
-                    <li className="menuItem">Privacy-Policy</li>
-                    <li className="menuItem">About</li>
-                    <li className="menuItem">Blog</li>
-                    <li className="menuItem">FAQ</li>
-                </ul>
-                <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
-                </div>
-                <div className="socialIcons">
-                    <span className="icon">
-                    <FaFacebookF />
-                    </span>
-                    <span className="icon">
-                    <a href="https://www.instagram.com/paarthavshah3697/" target="_blank"><FaInstagram /></a>
-                    </span>
-                    <span className="icon">
-                    <a href="https://github.com/Preetiraj3697" target="_blank"> <FaGithub /></a>
-                    </span>
-                    <span className="icon">
-                    <a href="https://www.linkedin.com/in/preetiraj3697/" target="_blank">  <FaLinkedin /></a>
-                    </span>
-                </div>
-            </ContentWrapper>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          © 2024 Chillax - Max Relax, Zero Drax. Where Entertainment Meets
+          Connection. Bringing movies, series, and music together for a
+          personalized, socially connected experience. Enjoy, explore, and
+          share—anytime, anywhere.
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <a href="" target="_blank">
+              <FaInstagram />
+            </a>
+          </span>
+          <span className="icon">
+            <a href="https://github.com/dpkgusain/Final_Minor" target="_blank">
+              {" "}
+              <FaGithub />
+            </a>
+          </span>
+          <span className="icon">
+            <a
+              href="https://www.linkedin.com/in/manya-0a9314256/"
+              target="_blank"
+            >
+              {" "}
+              <FaLinkedin />
+            </a>
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
